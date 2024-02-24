7fe6b050ab932909210611192ae21b56de265893
Andreas Tsatsanis <andtsa@ambtwo.local>
2024-02-24 12:25:12 +0100

    can testing, not quite working but we're cooking

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // #![feature(trait_alias)]
+#![allow(unused)]
 #![feature(type_alias_impl_trait)]
 #![no_std]
 #![no_main]
 
-mod tfwipanic;
+// mod tfwipanic;
 
 use core::cell::UnsafeCell;
 use cortex_m::interrupt::Mutex;
@@ -11,8 +12,8 @@ use defmt::export::panic;
 use embassy_executor::Spawner;
 use embassy_net::{Ipv4Address, Stack, StackResources};
 use embassy_net::tcp::TcpSocket;
-use embassy_stm32::{bind_interrupts, eth, peripherals, rng, Config};
-use embassy_stm32::eth::{Ethernet, PacketQueue};
+use embassy_stm32::{bind_interrupts, eth, peripherals, rng, Config, can, rcc};
+use embassy_stm32::eth::{Ethernet, InterruptHandler, PacketQueue};
 use embassy_stm32::eth::generic_smi::GenericSMI;
 use embassy_stm32::exti::ExtiInput;
 use embassy_stm32::gpio::{Flex, Input, Level, Output, Pin, Pull, Speed};
@@ -23,110 +24,149 @@ use static_cell::make_static;
 use embedded_io_async::Write;
 use defmt::*;
 use embassy_net::tcp::client::{TcpClient, TcpClientState};
-use embassy_stm32::peripherals::ETH;
+use embassy_stm32::peripherals::{ETH, FDCAN1};
 use embedded_nal_async::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpConnect};
 use rand_core::RngCore;
 
+use static_cell::StaticCell;
+
 // static RED_LED: Mutex<UnsafeCell<Option<Output<'static, Flex<dyn Pin<ExtiChannel=(), P=()>>>>>> = Mutex::new(UnsafeCell::new(None));
 
+// bind_interrupts!(struct Irqs {
+//     ETH => eth::InterruptHandler;
+//     RNG => rng::InterruptHandler<peripherals::RNG>;
+// });
+// bind_interrupts!(struct Irqs {
+//     FDCAN1_IT0 => ;
+//     // CAN3_RX1 => Rx1InterruptHandler<CAN3>;
+//     // CAN3_SCE => SceInterruptHandler<CAN3>;
+//     // CAN3_TX => TxInterruptHandler<CAN3>;
+// });
+
+// type Device = Ethernet<'static, embassy_stm32::peripherals::ETH, GenericSMI>;
+
+// #[embassy_executor::task]
+// async fn net_task(stack: &'static Stack<Device>) -> ! {
+//     stack.run().await
+// }
 bind_interrupts!(struct Irqs {
-    ETH => eth::InterruptHandler;
-    RNG => rng::InterruptHandler<peripherals::RNG>;
+    FDCAN1_IT0 => can::IT0InterruptHandler<FDCAN1>;
+    FDCAN1_IT1 => can::IT1InterruptHandler<FDCAN1>;
 });
-
-type Device = Ethernet<'static, embassy_stm32::peripherals::ETH, GenericSMI>;
-
-#[embassy_executor::task]
-async fn net_task(stack: &'static Stack<Device>) -> ! {
-    stack.run().await
-}
-
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
+    info!("\x1b[106;31m hello world! \x1b[0m");
+
+    // config.rcc.fdcan_clock_source = rcc::FdCanClockSource::HSE;
+    //
+    // let peripherals = embassy_stm32::init(config);
+    //
+    // let mut can = can::Fdcan::new(peripherals.FDCAN1, peripherals.PA11, peripherals.PA12, Irqs);
+    //
+    // can.can.apply_config(
+    //     can::config::FdCanConfig::default().set_nominal_bit_timing(can::config::NominalBitTiming {
+    //         sync_jump_width: 1.try_into().unwrap(),
+    //         prescaler: 8.try_into().unwrap(),
+    //         seg1: 13.try_into().unwrap(),
+    //         seg2: 2.try_into().unwrap(),
+    //     }),
+    // );
     let mut config = Config::default();
-    {
-        use embassy_stm32::rcc::*;
-        config.rcc.hsi = Some(HSIPrescaler::DIV1);
-        config.rcc.csi = true;
-        config.rcc.hsi48 = Some(Default::default()); // needed for RNG
-        config.rcc.pll1 = Some(Pll {
-            source: PllSource::HSI,
-            prediv: PllPreDiv::DIV4,
-            mul: PllMul::MUL50,
-            divp: Some(PllDiv::DIV2),
-            divq: None,
-            divr: None,
-        });
-        config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
-        config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
-        config.rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
-        config.rcc.voltage_scale = VoltageScale::Scale1;
-    }
-    let p = embassy_stm32::init(config);
-    let mut red_led = Output::new(p.PB14, Level::Low, Speed::VeryHigh);
-    let mut blue_led = Output::new(p.PB7, Level::Low, Speed::VeryHigh);
-    let mut green_led = Output::new(p.PB0, Level::Low, Speed::VeryHigh);
-    let mut button = ExtiInput::new(Input::new(p.PC13, Pull::Down), p.EXTI13);
+    config.rcc.hse = Some(rcc::Hse {
+        freq: embassy_stm32::time::Hertz(25_000_000),
+        mode: rcc::HseMode::Oscillator,
+    });
+    info!("\x1b[106;34m checkpoint 1 \x1b[0m");
+    config.rcc.fdcan_clock_source = rcc::FdCanClockSource::HSE;
+    info!("\x1b[106;34m checkpoint 2 \x1b[0m");
 
-    let mac_addr = [0x00, 0x00, 0x00, 0x69, 0x42, 0x00];
+    let peripherals = embassy_stm32::init(config);
+    info!("\x1b[106;34m checkpoint 3 \x1b[0m");
 
-    let device = Ethernet::new(
-        make_static!(PacketQueue::<16, 16>::new()), p.ETH, Irqs, p.PA1, p.PA2, p.PC1, p.PA7, p.PC4, p.PC5, p.PG13,
-        p.PB13, p.PG11, GenericSMI::new(0), mac_addr,
-    );
+    let mut can = can::FdcanConfigurator::new(peripherals.FDCAN1, peripherals.PD0, peripherals.PD1, Irqs);
+    info!("\x1b[106;34m checkpoint 4 \x1b[0m");
+
+    // 250k bps
+    can.set_bitrate(250_000);
+    info!("\x1b[106;34m checkpoint 5 \x1b[0m");
+
+    // let mut can = can.into_internal_loopback_mode();
+    // let mut can = can.into_internal_loopback_mode();
+    let mut can = can.into_normal_mode();
+    // let mut can = can.start(can::FdcanOperatingMode::BusMonitoringMode);
+    info!("\x1b[106;34m checkpoint 6 \x1b[0m");
 
 
+    info!("Configured");
 
 
-    let config = embassy_net::Config::dhcpv4(Default::default());
-    let mut rng = Rng::new(p.RNG, Irqs);
-    let mut seed = [0; 8];
-    rng.fill_bytes(&mut seed);
-    let seed = u64::from_le_bytes(seed);
-    let stack = &*make_static!(Stack::new(
-        device,
-        config,
-        make_static!(StackResources::<3>::new()),
-        seed
-    ));
+    let mut green_led = Output::new(peripherals.PB0, Level::High, Speed::Low);
+    let mut i = 0;
+    let mut last_read_ts = embassy_time::Instant::now();
 
-    unwrap!(spawner.spawn(net_task(&stack)));
-
-
-    stack.wait_config_up().await;
-
-    info!("Network task initialized");
-
-    let mut rx_buffer = [0; 1024];
-    let mut tx_buffer = [0; 1024];
-
-
+    info!("entering loop");
     loop {
-        // there needs to be a server running on the ground station on the below (static) ip
-        let remote_endpoint = (Ipv4Address::new(192, 168, 1, 4), 6942);
-        let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
+        let frame = can::frame::ClassicFrame::new_extended(0x123456F, &[i; 8]).unwrap();
+        info!("Writing frame");
+        _ = can.write(&frame).await;
+        info!("\x1b[106;34m checkpoint 7 \x1b[0m");
 
-        socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
-
-        info!("connecting...");
-        let r = socket.connect(remote_endpoint).await;
-        if let Err(e) = r {
-            info!("connect error: {:?}", e);
-            Timer::after_secs(1).await;
-            continue;
-        }
-        info!("connected!");
-        loop {
-            let r = socket.write_all(b"Hello World.\n").await;
-            if let Err(e) = r {
-                info!("write error: {:?}", e);
-                break;
+        match can.read().await {
+            Ok((rx_frame, ts)) => {
+                let delta = (ts - last_read_ts).as_millis();
+                last_read_ts = ts;
+                info!(
+                    "Rx: {:x} {:x} {:x} {:x} --- NEW {}",
+                    rx_frame.data()[0],
+                    rx_frame.data()[1],
+                    rx_frame.data()[2],
+                    rx_frame.data()[3],
+                    delta,
+                )
             }
-            Timer::after_secs(1).await;
+            Err(_err) => error!("Error in frame"),
         }
+
+        info!("\x1b[106;34m checkpoint 8 \x1b[0m");
+
+        Timer::after_millis(250).await;
+
+        i += 1;
+        if i > 3 {
+            break;
+        }
+    }
+
+    info!("\x1b[106;34m checkpoint 9: finished first loop \x1b[0m");
+
+
+    let (mut tx, mut rx) = can.split();
+    // With split
+    loop {
+        let frame = can::frame::ClassicFrame::new_extended(0x123456F, &[i; 8]).unwrap();
+        info!("Writing frame with split");
+        _ = tx.write(&frame).await;
+        info!("\x1b[106;34m checkpoint 10: split sent \x1b[0m");
+
+        match rx.read().await {
+            Ok((rx_frame, ts)) => {
+                let delta = (ts - last_read_ts).as_millis();
+                last_read_ts = ts;
+                info!(
+                    "Rx: {:x} {:x} {:x} {:x} --- NEW {}",
+                    rx_frame.data()[0],
+                    rx_frame.data()[1],
+                    rx_frame.data()[2],
+                    rx_frame.data()[3],
+                    delta,
+                )
+            }
+            Err(_err) => error!("Error in frame"),
+        }
+
+        Timer::after_millis(250).await;
+
+        i += 1;
     }
 }
